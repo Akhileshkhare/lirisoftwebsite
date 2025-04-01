c6243b4091e4f5d35d1ab0cec943467716ac24b4
akhilesh khare <akhileshkhare.work@gmail.com>
2025-04-01 23:17:12 +0530

    Fix: fixing the css issue.

--- src/pages/portfolio/Section1.tsx
+++ src/pages/portfolio/Section1.tsx
@@ -4,8 +4,8 @@ export default function Section1() {
 
   return (
     <section className="w-full bg-[#043A53] text-white text-center flex items-center justify-center flex-col h-auto md:h-[956px]">
-      <h1 className='font-normal md:text-5xl text-4xl mt-20 md:mt-5'>We help <span className='font-bold'>companies</span> turn their</h1>
-      <h1 className='font-normal md:text-5xl text-4xl my-4 my-8'><span className='font-bold'>ideas</span> ideas <span className='font-bold'>products</span></h1>
+      <h1 className='font-normal md:text-5xl text-3xl mt-20 px-10 md:px-0 md:mt-5'>We help <span className='font-bold'>companies</span> turn their</h1>
+      <h1 className='font-normal md:text-5xl text-3xl my-4 md:my-8'><span className='font-bold'>ideas</span> into <span className='font-bold'>products</span></h1>
       <img
       src="/Videoplaceholder.png"
       alt="Portfoli Lirisoft"
