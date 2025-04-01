0ef0c7cabec3e36b0f5a272b4814c0332031ad8b
akhilesh khare <akhileshkhare.work@gmail.com>
2025-04-01 23:08:22 +0530

    Fix: fixing the routing issue.

--- src/component/Navbar.tsx
+++ src/component/Navbar.tsx
@@ -43,7 +43,7 @@ const Navbar = () => {
       </button>
       {isOpen && (
         <div className="absolute top-16 left-0 w-full bg-white flex flex-col items-center gap-4 py-4 shadow-md md:hidden">
-          {["Home", "Services","Product", "Portfolio", "About", "Contact"].map((item) => (
+          {["Home","About", "Services","Product","Carrer", "Portfolio",  "Contact"].map((item) => (
             <Link key={item} to={`/${item.toLowerCase()}`}            className="relative hover:text-blue-900 after:content-[''] after:absolute after:left-0 after:bottom-[-2px] after:w-full after:h-[2px] after:bg-blue-700 after:scale-x-0 hover:after:scale-x-100 after:transition-transform after:duration-300"
 >
               {item}
